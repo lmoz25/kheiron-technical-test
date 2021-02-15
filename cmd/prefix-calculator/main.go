08f6f45aa0510084ddaf69b92fbe52db528475f2
Liam Morris <liammorris971@gmail.com>
2021-02-15 21:24:07 +0000

    Module building fixed, user input fixed

--- cmd/prefix-calculator/main.go
+++ cmd/prefix-calculator/main.go
@@ -1,33 +1,36 @@
-package calculator
+package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 
 	calculator "gitlab.com/lmoz25/kheiron-technical-test/internal/prefix-calculator"
 )
 
 func main() {
-	var calc calculator.Calculator
-	// Println function is used to
-	// display output in the next line
-	fmt.Println("Enter sum in prefix notation: ")
+	for {
+		var calc calculator.Calculator
+		// Println function is used to
+		// display output in the next line
+		fmt.Println("Enter sum in prefix notation: ")
 
-	// var then variable name then variable type
-	var sum string
+		in := bufio.NewReader(os.Stdin)
+		sum, err := in.ReadString('\n')
 
-	// Taking input from user
-	fmt.Scanln(&sum)
+		fmt.Printf("Sum: %s\n", sum)
 
-	err := calc.ParseInput(sum)
-	if err != nil {
-		fmt.Println("Error: ", err)
-		return
+		err = calc.ParseInput(sum)
+		if err != nil {
+			fmt.Println("Error: ", err)
+			return
+		}
+		result, err := calc.Result()
+		if err != nil {
+			fmt.Println("Error: ", err)
+			return
+		}
+
+		fmt.Printf("Result: %.1f\n", result)
 	}
-	result, err := calc.Result()
-	if err != nil {
-		fmt.Println("Error: ", err)
-		return
-	}
-
-	fmt.Println(result)
 }
